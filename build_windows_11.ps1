d5a482855fc3502a53e9f3214a27e167939c7ca2
Jodie Cunningham <jodie.cunningham@gmail.com>
2023-01-28 21:44:54 -0600

    updating naming, disk sizes and output directories

--- build_windows_11.ps1
+++ build_windows_11.ps1
@@ -1,4 +1,4 @@
 $env:PACKER_LOG=1
 $env:PACKER_LOG_PATH="packerlog.txt"
 
-packer build --only=hyperv-iso --var iso_url="G:\os\Win11_22H2_English_x64v1.iso" -force .\windows_11.json
+packer build --var iso_url="G:\os\Win11_22H2_English_x64v1.iso" -force .\windows_11.json
